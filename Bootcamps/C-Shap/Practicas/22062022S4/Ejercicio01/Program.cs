54cfd0fd6636d1dd978ef5be3ae7b24353b75afe
Feliz Florian Jose Luis <joseluisfelizflorian@hotmail.com>
2022-07-06 19:27:55 -0400

    Modificacion del Ejercicio 22062022S4
    Se agregaron los siguientes procedmientos:
    -voidInicializarValores()
    -intNumerosAleatores()
    -voidBubbleSort()
    
    Co-Authored-By: Feliz Florian Jose Luis <joseluisfelizflorian@hotmail.com>

--- Bootcamps/C-Shap/Practicas/22062022S4/Ejercicio01/Program.cs
+++ Bootcamps/C-Shap/Practicas/22062022S4/Ejercicio01/Program.cs
@@ -9,5 +9,54 @@
 ///	            *Imprimir el arreglo antes y despues de ordenado.
 ///<summary>
 
+Console.WriteLine("Favor de ingresar el tamano del arreglo");
+int.TryParse(Console.ReadLine(), out int intPosiciones);
+
+int [] intArray = new int[intPosiciones];
+
+voidInicializarValores(intArray);
+
+voidBubbleSort(intArray);
+
+void voidInicializarValores(int [] paramArrey)
+{
+    for(int i=0; i < paramArrey.Length ; i++)
+    {
+        
+        Console.Write(paramArrey[i]=intNumerosAleatores());
+    }
+}
+
+int intNumerosAleatores()
+{
+    Random intNumeroAleatoreos = new Random();
+    
+    return intNumeroAleatoreos.Next(1,100);
+}
+
+void voidBubbleSort(int []paramArray)
+{
+    int intContenedor;
+
+    for(int i=1; i <= paramArray.Length -1; i++)
+    {
+        for(int j = 0;j< paramArray.Length -1;  j++ )
+        {
+            if (paramArray[j + 1] < paramArray[j])
+            {
+                intContenedor = paramArray[j];
+                paramArray[j] = paramArray[j+1];
+                paramArray[j+1]=intContenedor;
+            }
+        }
+    }
+                //Sorted array  
+            foreach (int num in paramArray)  
+            {  
+                Console.Write("\t {0}",num);  
+            }  
+            Console.Read();
+}
+
 
 #endregion Ejercicio 01
