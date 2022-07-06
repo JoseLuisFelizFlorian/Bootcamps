83097ef28026c77d0673984791d0e5534c317ac5
Feliz Florian Jose Luis <joseluisfelizflorian@hotmail.com>
2022-07-05 22:20:09 -0400

    Ejercicio02 15062022S3
    Hacer un programa que imprima los numeros impares entre el 1 al 1000
    
    Co-Authored-By: Feliz Florian Jose Luis <joseluisfelizflorian@hotmail.com>

---
+++ Bootcamps/C-Shap/Practicas/15062022S3/Ejercicio02/Program.cs
@@ -0,0 +1,17 @@
+﻿#region Ejercicio 03
+///<summary>
+/// Autor: Feliz Florian Jose Luis
+/// Fecha: 11/06/2022
+/// Descripcion: Hacer un programa que imprima los numeros impares entre el 1 al 1000
+///<summary>
+
+
+for (int i = 1; i <= 1000; i++)
+//Valida si el residuo de un numero es 0
+    if(i % 2 != 0)
+    //Imprime los numeros impares 1,3,5...999
+        Console.WriteLine(i);
+
+
+#endregion Ejercicio 03
+
