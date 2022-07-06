5ea391f398dfed229972bd5d361803af124ce3ba
Feliz Florian Jose Luis <joseluisfelizflorian@hotmail.com>
2022-07-05 22:23:24 -0400

    Ejercicio01 22062022S4
    *Crear un arreglo de 20 posiciones.
     *Llenar el arreglo con valores aleatorios.
     *Organizar de manera ascendente con el metodo de ordenacion burbuja.
    *Imprimir el arreglo antes y despues de ordenado.
    
    Co-Authored-By: Feliz Florian Jose Luis <joseluisfelizflorian@hotmail.com>

---
+++ Bootcamps/C-Shap/Practicas/22062022S4/Ejercicio01/Program.cs
@@ -0,0 +1,13 @@
+﻿#region Ejercicio 01
+///<summary>
+/// Autor: Feliz Florian Jose Luis
+/// Fecha: 22/06/2022
+/// Descripcion: 	
+///             *Crear un arreglo de 20 posiciones.
+///         	*Llenar el arreglo con valores aleatorios.
+///	            *Organizar de manera ascendente con el metodo de ordenacion burbuja.
+///	            *Imprimir el arreglo antes y despues de ordenado.
+///<summary>
+
+
+#endregion Ejercicio 01
