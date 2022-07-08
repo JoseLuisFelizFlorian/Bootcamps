70899a1d686e839fee94ef3b42f26a727ec576f4
Feliz Florian Jose Luis <joseluisfelizflorian@hotmail.com>
2022-07-08 19:33:47 -0400

    Modificacion del ejercicio 1 22062022S4
    Co-Authored-By: Feliz Florian Jose Luis <joseluisfelizflorian@hotmail.com>

--- Bootcamps/C-Shap/Practicas/22062022S4/Ejercicio01/Program.cs
+++ Bootcamps/C-Shap/Practicas/22062022S4/Ejercicio01/Program.cs
@@ -2,6 +2,7 @@
 ///<summary>
 /// Autor: Feliz Florian Jose Luis
 /// Fecha: 22/06/2022
+/// Codgio: 22062022S4
 /// Descripcion: 	
 ///             *Crear un arreglo de 20 posiciones.
 ///         	*Llenar el arreglo con valores aleatorios.
@@ -9,32 +10,64 @@
 ///	            *Imprimir el arreglo antes y despues de ordenado.
 ///<summary>
 
-Console.WriteLine("Favor de ingresar el tamano del arreglo");
+Console.Write("Favor de ingresar el tamaño del arreglo: ");
 int.TryParse(Console.ReadLine(), out int intPosiciones);
 
+//Declaracion y asignacion del tamaño del arreglo
 int [] intArray = new int[intPosiciones];
 
-voidInicializarValores(intArray);
+//Inicializa un arreglo con valores aleatorios
+voidInicializarArreglo(intArray);
 
-voidBubbleSort(intArray);
+Console.WriteLine("\nPresione las siguientes teclas para ordenar: \n[A] Ascendente \n[D] Descendente");
+string stringTipoOrdenamiento = Console.ReadLine();
 
-void voidInicializarValores(int [] paramArrey)
-{
-    for(int i=0; i < paramArrey.Length ; i++)
-    {
-        
-        Console.Write(paramArrey[i]=intNumerosAleatores());
-    }
-}
+//Ordena el arreglo de forma ascendente o desendente
+//segun el tipo de ordenamiento ingresedo por el usuario
+voidBubbleSort(intArray, stringTipoOrdenamiento);
 
-int intNumerosAleatores()
+//Salida del arreglo segun el tipo de ordenamiento seleccionado
+voidImprimirValores(intArray);
+
+#region Genera numeros aleatorios
+/// <summary>
+/// Genera numeros aleatoreos comprendidos del 1 al 100.
+/// Algunos numeros pueden repetirse.
+/// </summary>
+/// <returns>Valor intenger de 2 digitos</returns>
+int intFuctionNumerosAleatores()
 {
     Random intNumeroAleatoreos = new Random();
     
     return intNumeroAleatoreos.Next(1,100);
 }
 
-void voidBubbleSort(int []paramArray)
+#endregion Genera numeros aleatorios
+
+#region Inicializar Array
+/// <summary>
+/// Inicializa un array con numeros aleatoreos del 1 al 100
+/// </summary>
+/// <param name="paramArray">Array de tipo intenger</param>
+/// <returns>Objeto tipo array<returns>
+void voidInicializarArreglo(int [] paramArray)
+{
+    Console.Write("Valores Generado: ");
+    for(int i=0; i < paramArray.Length ; i++)
+    {      
+        Console.Write($"{paramArray[i]=intFuctionNumerosAleatores()}\t  ");
+    }
+}
+#endregion Inicializar Array
+
+#region Metodo de ordenamiento
+/// <summary>
+/// Metodo de ordenamiento burbuja.
+/// Ordena los valores de forma ascendente y descendente.
+/// </summary>
+/// <param name="paramArray">Recibe un arreglo de tipo intenger</param>
+/// <param name="paramStringOrden">Opcional: [A] Ascendente [D] Descendente</param>  
+void voidBubbleSort(int []paramArray, string  paramStringOrden ="A")
 {
     int intContenedor;
 
@@ -42,21 +75,41 @@ void voidBubbleSort(int []paramArray)
     {
         for(int j = 0;j< paramArray.Length -1;  j++ )
         {
-            if (paramArray[j + 1] < paramArray[j])
+            if (paramArray[j + 1] < paramArray[j] && paramStringOrden =="A")
             {
                 intContenedor = paramArray[j];
                 paramArray[j] = paramArray[j+1];
                 paramArray[j+1]=intContenedor;
             }
+            else
+            {
+                if (paramArray[j + 1] > paramArray[j])
+                {
+                    intContenedor = paramArray[j];
+                    paramArray[j] = paramArray[j+1];
+                    paramArray[j+1]=intContenedor;
+                }
+            }
         }
     }
-                //Sorted array  
-            foreach (int num in paramArray)  
-            {  
-                Console.Write("\t {0}",num);  
-            }  
-            Console.Read();
 }
+#endregion Metodo de ordenamiento
 
+#region Imprime los valores ordenado
+/// <summary>
+/// Imprime los valores almecenado en un arreglo de tipo intenger
+/// </summary>
+/// <param name="paramArray">Recibe un arreglo de tipo intenger</param>
+void voidImprimirValores(int []paramArray)
+{   
+    Console.Write("\nValores Ordenado: ");
+    foreach (int intArray in paramArray)  
+    {  
+        Console.Write("{0:00}\t  ",intArray);  
+    }  
+    Console.Read();
+    Console.Clear();
+}
+#endregion Imprime los valores ordenado
 
 #endregion Ejercicio 01
